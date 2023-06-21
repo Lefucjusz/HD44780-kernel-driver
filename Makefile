75caa45072676c3bc32989b362f7aa0137fa3b19
Lefucjusz <elektromarcin@gmail.com>
2023-06-22 00:03:50 +0200

    Add simple buffer functionality
    Added simple buffer functionality to test
    if the device registration works properly.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 obj-m += HD44780_driver.o
 HD44780_driver-objs += driver_main.o file_operations.o
-ccflags-y := -std=gnu99 -O2
+ccflags-y := -std=gnu99 -O2 -Wno-declaration-after-statement
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
--- file_operations.c
+++ file_operations.c
@@ -1,6 +1,12 @@
 #include "file_operations.h"
 #include "log.h"
 
+#define MIN(x,y) (((x) < (y)) ? (x) : (y))
+
+#define DATA_BUFFER_SIZE 1024
+char data_buffer[DATA_BUFFER_SIZE + 1];
+
+
 static int file_open(struct inode *inode, struct file *file)
 {
     LOG_INFO("Opening file");
@@ -13,16 +19,42 @@ static int file_release(struct inode *inode, struct file *file)
     return 0;
 }
 
-static ssize_t file_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+static ssize_t file_read(struct file *file, char __user *buffer, size_t size, loff_t *offset)
 {
     LOG_INFO("Reading from file");
-    return 0;
+
+    const ssize_t bytes_to_copy = MIN(strlen(data_buffer) - *offset, size);
+    if (bytes_to_copy <= 0) {
+        return 0;
+    }
+
+    const size_t bytes_not_copied = copy_to_user(buffer, data_buffer, bytes_to_copy);
+    if (bytes_not_copied > 0) {
+        LOG_ERROR("Failed to copy %zuB/%zuB to user!", bytes_not_copied, bytes_to_copy);
+        return -EFAULT;
+    }
+
+    *offset += bytes_to_copy;
+    return bytes_to_copy;
 }
 
-static ssize_t file_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+static ssize_t file_write(struct file *file, const char __user *buffer, size_t size, loff_t *offset)
 {
     LOG_INFO("Writing to file");
-    return 0;
+
+    if (size > DATA_BUFFER_SIZE) {
+        size = DATA_BUFFER_SIZE;
+    }
+
+    const size_t bytes_not_copied = copy_from_user(data_buffer, buffer, size);
+    if (bytes_not_copied > 0) {
+        LOG_ERROR("Failed to copy %zuB/%zuB from user!", bytes_not_copied, size);
+        return -EFAULT;
+    }
+
+    data_buffer[size] = '\0';
+
+    return size;
 }
 
 struct file_operations *get_file_operations(void)
